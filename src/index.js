76a3d984281709f9801ad8959033dc67ae3197c4
soldier <sodier@dcpt.online>
2018-05-18 19:37:35 +0800

    eslint 去掉

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+const a =1;
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
